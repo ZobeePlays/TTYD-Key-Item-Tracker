aeaf32d9d7659eff6955e48e1cfbdc2e162154ef
ZobeePlays <159688147+ZobeePlays@users.noreply.github.com>
2025-03-09 06:07:46 -0500

    add files (no images yet funny)

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,14 @@
+ITEM_MAPPING = {
+	[77772007] = {{"Autograph", "toggle"}},
+	[77772008] = {{"BlackKey1", "toggle"}},
+	[77772009] = {{"BlackKey2", "toggle"}},
+	[77772010] = {{"BlackKey3", "toggle"}},
+	[77772011] = {{"BlackKey4", "toggle"}},
+	[77772012] = {{"Blanket", "toggle"}},
+	[77772013] = {{"BlimpTicket", "toggle"}},
+	[77772014] = {{"BlueKey", "toggle"}},
+	[77772015] = {{"BoatCurse", "toggle"}},
+	[77772016] = {{"Bobbery", "toggle"}},
+	[77772018] = {{"Briefcase", "toggle"}},
+# do the rest later lol
+}
---
+++ scripts/autotracking/location_mapping.lua
@@ -0,0 +1,6 @@
+LOCATION_MAPPING = {
+	[78780000] = {"@Rogueport Docks/HP Drain"},
+	[78780001] = {"@Rogueport Docks/Star Piece"},
+	[78780706] = {"@Rogueport Docks/Star Piece 2"},
+# do the rest later lol
+}
